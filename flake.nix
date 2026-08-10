aa4892aebdac80d6bf1715bb49a8352c028daf66
Sven van Heugten <sven@memcmp.org>
2026-08-10 07:04:41 +0200

    Bump version to v0.4.0

--- flake.nix
+++ flake.nix
@@ -53,7 +53,7 @@
 
         packages.default = pkgs.stdenv.mkDerivation {
           pname = "gh-post-range-diff";
-          version = "0.3.0";
+          version = "0.4.0";
           src = self;
           meta.mainProgram = "gh-post-range-diff";
 
